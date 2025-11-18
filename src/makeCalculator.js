90b2163f5e247a093b2cd0d35b12d4c65f34c8e2
maksbtw <maks.20050817@gmail.com>
2025-11-18 23:11:03 +0100

    ready to check

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,42 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    currentValue: 0,
+    get add() {
+      return (value) => {
+        this.currentValue += value;
+      };
+    },
+    get subtract() {
+      return (value) => {
+        this.currentValue -= value;
+      };
+    },
+    get multiply() {
+      return (value) => {
+        this.currentValue *= value;
+      };
+    },
+    get divide() {
+      return (value) => {
+        this.currentValue /= value;
+      };
+    },
+    get result() {
+      return this.currentValue;
+    },
+    operate(callback, value) {
+      callback(value);
+
+      return this;
+    },
+    reset() {
+      this.currentValue = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
